3b2d5b88f7418cbc75ab40b69745e56637306be7
Pasztor Arthur <apasztor.dev@gmail.com>
2022-10-18 12:17:13 +0300

    Build Players Screen (for now without players' list)

--- app/src/main/java/com/denisatrif/truthdare/compose/ComposePlayersScreen.kt
+++ app/src/main/java/com/denisatrif/truthdare/compose/ComposePlayersScreen.kt
@@ -1,13 +1,106 @@
 package com.denisatrif.truthdare.compose
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.denisatrif.truthdare.R
+import com.denisatrif.truthdare.ui.theme.PrimaryColor
+import com.denisatrif.truthdare.ui.theme.SecondaryColor
 
+@Preview
 @Composable
 fun ComposablePlayersScreen() {
+    val painterGirl = painterResource(id = R.drawable.girl_2)
+    val descGirl = "CD Girl"
+    val painterBoy = painterResource(id = R.drawable.boy_2)
+    val descBoy = "CD Boy"
+
     Box(modifier = Modifier.fillMaxSize()) {
         Background()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f, true)
+            ) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(90.dp)
+                        .padding(vertical = 12.dp, horizontal = 8.dp),
+                    shape = RoundedCornerShape(30.dp)
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(SecondaryColor),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            modifier = Modifier.padding(start = 8.dp),
+                            painter = painterGirl,
+                            contentDescription = descGirl,
+                            contentScale = ContentScale.Crop
+                        )
+                        Image(
+                            modifier = Modifier.padding(start = 8.dp),
+                            painter = painterBoy,
+                            contentDescription = descBoy,
+                            contentScale = ContentScale.Crop
+                        )
+                        Text(
+                            text = stringResource(id = R.string.add_a_new_player),
+                            fontSize = 20.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.Normal,
+                            textAlign = TextAlign.Start,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 8.dp)
+                        )
+                    }
+                }
+            }
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(110.dp),
+                shape = RoundedCornerShape(topStartPercent = 80)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(PrimaryColor),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.all_right_everything_set),
+                        fontSize = 24.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.wrapContentSize()
+                    )
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/denisatrif/truthdare/ui/theme/Type.kt
+++ app/src/main/java/com/denisatrif/truthdare/ui/theme/Type.kt
@@ -2,13 +2,19 @@ package com.denisatrif.truthdare.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.denisatrif.truthdare.R
+
+val fontFamilyMontserrat = FontFamily(
+    Font(R.font.montserrat_alternates_thin, FontWeight.Thin)
+)
 
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamilyMontserrat,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
